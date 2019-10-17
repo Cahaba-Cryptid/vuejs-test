4100d07eb3d328dddd76a1a4d19df96e55ecb1ae
Hayden <chfrank9011@gmail.com>
2019-10-17 12:21:20 -0500

    Created two components for a header and jumbotron for displaying questions. API calls can be made to a randomly generated API by opentdb.com.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+
+    <b-container class="bv-example-row">
+  <b-row>
+    <b-col sm="6" offset="3">
+      <QuestionBox 
+      <!-- passing questions index to component with shorthand v-bind -->
+      :question="questions[index]"
+      />
+      </b-col>
+  </b-row>
+</b-container>
+
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue';
+import QuestionBox from './components/QuestionBox';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header,
+    QuestionBox
+  },
+  data() {
+    return {
+      questions: [],
+      index: 0
+    }
+  },
+  mounted: function() {
+    fetch('https://opentdb.com/api.php?amount=10&category=9&type=multiple', {
+      method: 'get'
+    })
+    .then((response) => {
+      return response.json()
+    })
+    .then((jsonData) => {
+      this.questions = jsonData.results
+    })
   }
 }
+
 </script>
 
 <style>
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="question-box-container">
+    <b-jumbotron>
+
+      <template v-slot:lead>
+        Questions go here.
+      </template>
+
+      <hr class="my-4" />
+
+      <p>
+        Answer list goes here.
+      </p>
+
+      <b-button variant="primary" href="#">Submit!</b-button>
+      <b-button variant="success" href="#">Next</b-button>
+    </b-jumbotron>
+  </div>
+</template>
